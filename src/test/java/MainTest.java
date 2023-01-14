e1fa7cccc295acd4fcb3c16bc59c201b47f3c1dc
kirgintsevaa <kirgintseva@tutu.tech>
2023-01-14 14:19:39 +0300

    1

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,24 +1,32 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.Set;
+import java.time.Duration;
 
-public class MainTest extends BaseTest {
-
+public class MainTest {
     private final String login = "nowaj54360@lubde.com";
     private final String password = "Nowaj54360@lubde.com";
-    private final String find = "ОТУС";
-    private final String expectedResult = "Онлайн‑курсы для профессионалов, дистанционное обучение современным ...";
+    WebDriver driver;
+
+    @AfterEach
+    public void close() {
+        if (driver != null)
+            driver.quit();
+    }
 
     @Test
-    public void firstTask() throws InterruptedException {
+    public void firstTask() {
 
 //1. Открыть Chrome в headless режиме
         WebDriverManager.chromedriver().setup();
@@ -28,15 +36,14 @@ public class MainTest extends BaseTest {
         driver.manage().window().maximize();
 //2. Перейти на https://duckduckgo.com/
         driver.get("https://duckduckgo.com/");
-        Thread.sleep(2000);
-//3. В поисковую строку ввести ОТУС
+        //3. В поисковую строку ввести ОТУС
         enterString();
 //4. Проверить что в поисковой выдаче первый результат Онлайн‑курсы для профессионалов, дистанционное обучение
         checkResult();
     }
 
     @Test
-    public void secondTask() throws InterruptedException {
+    public void secondTask() {
 
 //1. Открыть Chrome в режиме киоска
         WebDriverManager.chromedriver().setup();
@@ -44,55 +51,52 @@ public class MainTest extends BaseTest {
         driver.manage().window().fullscreen();
 //2. Перейти на https://demo.w3layouts.com/demos_new/template_demo/03-10-2020/photoflash-liberty-demo_Free/685659620/web/index.html?_ga=2.181802926.889871791.1632394818-2083132868.1632394818
         driver.get("https://demo.w3layouts.com/demos_new/template_demo/03-10-2020/photoflash-liberty-demo_Free/685659620/web/index.html?_ga=2.181802926.889871791.1632394818-2083132868.1632394818");
-        Thread.sleep(2000);
-//3. Нажать на любую картинку
+        //3. Нажать на любую картинку
         driver.findElement(By.cssSelector(".content-overlay")).click();
-        Thread.sleep(2000);
-//4. Проверить что картинка открылась в модальном окне
+        //4. Проверить что картинка открылась в модальном окне
         driver.findElement(By.cssSelector(".pp_hoverContainer")).isDisplayed();
     }
 
     @Test
-    public void thirdTask() throws InterruptedException {
-        Logger logger = LogManager.getLogger(BaseTest.class);
+    public void thirdTask() {
+        Logger logger = LogManager.getLogger(String.valueOf(MainTest.class));
 //1. Открыть Chrome в режиме полного экрана
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
 //2. Перейти на https://otus.ru
         driver.get("https://otus.ru/");
-        Thread.sleep(2000);
-//3. Авторизоваться под каким-нибудь тестовым пользователем(можно создать нового)
+        //3. Авторизоваться под каким-нибудь тестовым пользователем(можно создать нового)
         loginInOtus();
 //4. Вывести в лог все cookie
-        Set<Cookie> cookies;
-        cookies = driver.manage().getCookies();
-        //System.out.println(cookies);
-        logger.info(cookies);
+        logger.info(driver.manage().getCookies().toString());
     }
 
-    private void enterString() throws InterruptedException {
+    private void enterString() {
+        String find = "ОТУС";
         driver.findElement(By.cssSelector("#search_form_input_homepage")).sendKeys(find);
-        Thread.sleep(2000);
         driver.findElement(By.cssSelector("#search_button_homepage")).click();
-        Thread.sleep(2000);
     }
 
     private void checkResult() {
-        Assertions.assertEquals(expectedResult, driver.findElement(By.cssSelector(".ikg2IXiCD14iVX7AdZo1")).getText());
+        String expectedResult = "Онлайн‑курсы для профессионалов, дистанционное обучение современным ...";
+        String actualResult = driver.findElement(By.cssSelector("#r1-0>div+div span")).getText();
+        Assertions.assertEquals(expectedResult, actualResult);
     }
 
-    private void loginInOtus() throws InterruptedException {
+    private void loginInOtus() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         driver.findElement(By.cssSelector("button.js-cookie-accept.cookies__button")).click();
-        Thread.sleep(2000);
         driver.findElement(By.cssSelector(".header3__button-sign-in")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.cssSelector("input[type = 'text']")).sendKeys(login);
-        Thread.sleep(2000);
+        try {
+            wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".header3__button-sign-in"))));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("form[action='/login/']>div+div input[type='text']")));
+        element.sendKeys(login);
         driver.findElement(By.cssSelector("input[type = 'password']")).sendKeys(password);
-        Thread.sleep(2000);
-        driver.findElement(By.cssSelector("button[class='new-button new-button_full new-button_blue new-button_md']")).click();
-        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("form[action='/login/']>div+div+div+div>button")).click();
     }
 }
 
